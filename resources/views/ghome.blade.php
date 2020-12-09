393fcf2af5d13e4954cb80c8a5f582cff28608cd
sk-123899 <kotkarshreyas2000@gmail.com>
2020-12-09 18:03:58 +0530

    Email verification

--- resources/views/ghome.blade.php
+++ resources/views/ghome.blade.php
@@ -8,8 +8,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-
-         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <img src="./images/logo1.jpg" style="width:2.5%; height:2%;">
             <p class="navbar-brand"> City Infrastructure </p>
             <ul class="navbar-nav">
